128e23d6194b6dd45d40fcee0793d41905ea14bc
cds-amal <cds.sudama@gmail.com>
2024-06-11 19:59:12 -0400

    Update to correct package

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 # Remove the existing go.sum file
 rm ./go.sum >/dev/null 2>&1
 
-GOPRIVATE=github.com/DIN-center go get github.com/cds-amal/go-sample
+GOPRIVATE=github.com/DIN-center go get github.com/DIN-center/din-sc
 go run .
-
